5d816a1e71a77950dbe502aaa885547fa436d99b
amish thapa <thapaamish805@gmail.com>
2024-12-11 14:56:38 +0545

    completed login page and created signup page

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -6,3 +6,12 @@ function checkInput() {
     alert("enter all the fields");
   }
 }
+
+function showPassword() {
+  let password = document.getElementById("password");
+  if (password.type === "password") {
+    password.type = "text";
+  } else {
+    password.type = "password";
+  }
+}
