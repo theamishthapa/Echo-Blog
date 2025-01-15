f43b422f71eda3230d22682a33ca54e3edf29e36
rujenmrj <rujenmrj15@gmail.com>
2025-01-15 08:05:58 +0545

    fixed typos in sql

--- backend/echo.sql
+++ backend/echo.sql
@@ -1,20 +1,22 @@
- CREATE DATABASE IF NOT EXISTS echo;
- use echo;
- CREATE TABLE IF NOT EXISTS users(
-     user_id varchar(255) PRIMARY KEY,
- 	 username VARCHAR(255) UNIQUE,
-     email VARCHAR(255) UNIQUE,
-     `password` VARCHAR(255),
-     active_status BOOLEAN DEFAULT FALSE,
-     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-     updatated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-     profile_pic VARCHAR(255) UNIQUE
- );
- CREATE TABLE IF  NOT EXISTS post(
-     id varchar(255) PRIMARY KEY,
- 	username VARCHAR(255),
-     Caption TEXT,
-     posted_date DATETIME,
-     image varchar,
-     FOREIGN KEY post(username) REFERENCES users(username) ON UPDATE CASCADE ON DELETE CASCADE
- );
+CREATE DATABASE IF NOT EXISTS echo;
+USE echo;
+
+CREATE TABLE IF NOT EXISTS users (
+    user_id VARCHAR(255) PRIMARY KEY,
+    username VARCHAR(255) UNIQUE,
+    email VARCHAR(255) UNIQUE,
+    `password` VARCHAR(255),
+    active_status BOOLEAN DEFAULT FALSE,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    profile_pic VARCHAR(255) UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS post (
+    id VARCHAR(255) PRIMARY KEY,
+    user_id VARCHAR(255),
+    caption TEXT,
+    posted_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    image VARCHAR(255),
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON UPDATE CASCADE ON DELETE CASCADE
+);
